b8707edadb1f4774b8b87b269fb49be86a0ad814
smash-cracker <dennythomas069@gmail.com>
2026-05-15 10:52:01 +0530

    feat: holiday and sunday awareness

--- present.py
+++ present.py
@@ -1,5 +1,6 @@
 import requests
 import sys
+from datetime import datetime
 
 BASE_URL = "https://archeios.cezentech.com"
 
@@ -10,6 +11,39 @@ if len(sys.argv) < 3:
 
 email = sys.argv[1]
 password = sys.argv[2]
+
+# Current date in DD-MM-YY format
+today = datetime.now().strftime("%d-%m-%y")
+
+# Holidays to skip
+HOLIDAYS = {
+    "01-01-26",  # NEW YEAR
+    "14-01-26",  # MAKARA SANKRANTI
+    "26-01-26",  # REPUBLIC DAY
+    "19-03-26",  # UGADI FESTIVAL
+    "21-03-26",  # RAMZAN
+    "03-04-26",  # GOOD FRIDAY
+    "15-04-26",  # VISHU
+    "01-05-26",  # MAY DAY
+    "28-05-26",  # BAKRID
+    "15-08-26",  # INDEPENDENCE DAY
+    "26-08-26",  # ONAM
+    "14-09-26",  # GANESH CHATHURTHI
+    "02-10-26",  # GANDHI JAYANTHI
+    "31-10-26",  # KANNADA RAJYOTSAV
+    "10-11-26",  # DIWALI
+    "25-12-26",  # CHRISTMAS DAY
+}
+
+# Skip Sundays
+if datetime.now().weekday() == 6:
+    print("Today is Sunday. Skipping attendance.")
+    sys.exit(0)
+
+# Skip holidays
+if today in HOLIDAYS:
+    print(f"Today ({today}) is a holiday. Skipping attendance.")
+    sys.exit(0)
 
 # LOGIN
 login_response = requests.post(
